9c4f7781a3a476b8a5d9413c2feb3c9b3ad057a7
Goran Janes <ran3go@gmail.com>
2024-04-18 20:43:18 +0200

    Inicijalno dodavanje ReactJS-Zadatak v1 koda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import my_data from './custom_utils/default_module';
+import * as operacija from "./custom_utils/named_module";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Osnovni podaci o meni</h2>
+      <p>Moje ime je: {my_data.ime}</p>
+      <p>Moje prezime je: {my_data.prezime}</p>
+      <p>Moje prebivalište je: {my_data.prebivaliste}</p>
+      <h2>Osnovne računske operacije</h2>
+      <p>Suma brojeva 6 i 7 je: {operacija.sum(6,7)}</p>
+      <p>Razlika brojeva 56 i 20 je: {operacija.sub(56,20)}</p>
+      <p>Rezultat dijeljenja brojeva 56 i 7 je: {operacija.div(56,7)}</p>
+      <p>Umnožak brojeva 6 i 7 je: {operacija.multpl(6,7)}</p>
     </div>
   );
 }
---
+++ src/custom_utils/default_module.js
@@ -0,0 +1,6 @@
+const my_data = {
+    ime: "Goran",
+    prezime: "Janeš",
+    prebivaliste: "Zagreb"
+};
+export default my_data;
